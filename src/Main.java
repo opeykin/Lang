3b42a9e2a6eb5ee630ef86f6bc01a8677b1a681e
Opeykin Alexander <coinmbx@gmail.com>
2012-03-01 18:07:06 +0400

    RC1

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,19 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
-	
-	public static void main(String[] args) {
+
+    public static void main(String[] args) {
 
         Map<String, Statement> bindings = new HashMap<String, Statement>();
 
-        String fileName = "name.txt";
+        if (args.length == 0) {
+            System.err.println("Usage: prog imput_file_name");
+            System.exit(1);
+        }
+        String fileName = args[0];
         List<String> tokens = null;
         try {
-             tokens = SourceReader.read(fileName);
+            tokens = SourceReader.read(fileName);
         } catch (Exception e) {
             System.err.println(e.getMessage());
             System.exit(1);
@@ -23,22 +27,6 @@ public class Main {
 
         Parser p = new Parser(tokens, bindings);
         Statement st = p.parse();
-            System.out.println(st.evaluate().getString());
-            System.out.println(st.getString());
-
-//		IntegerConstant one = new IntegerConstant(1);
-//
-//		NameHolder funName = new NameHolder("fact", bindings);
-//		NameHolder x = new NameHolder("x", bindings);
-//
-//		Statement s = new IF(x, new Mul(x, new Apply(funName, new Minus(x, one))), one);
-//
-//		Fun fun = new Fun(s, x);
-//		funName.bind(fun);
-//		//Apply prog = new Apply(one, ten);
-//		Apply prog = new Apply(fun, new IntegerConstant(3));
-//
-//		System.out.println(prog.getString());
-//		System.out.println(prog.evaluate().getString());
-	}
+        System.out.println(st.evaluate().getString());
+    }
 }
